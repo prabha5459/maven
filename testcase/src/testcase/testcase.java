a66302fbd5249991bb272c7b91b5afe7c85bbf11
bharatamprabhakar <bharatamprabhakar@gmail.com>
2019-07-23 11:18:30 +0100

    test case to run maven project

---
+++ testcase/src/testcase/testcase.java
@@ -0,0 +1,26 @@
+package testcase;
+
+public class testcase {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int x = 25;
+//		x += 4; //x=x+4 , *(x=x*4), /(x=x/4)
+//		System.out.println(x);
+		
+		int x,y,z,a,b,c,d;
+      x = 25;
+		y = 10;
+		z = x + y;
+		a = x - y;
+		b = x / y;
+		c = x * y;
+		d = x % y;
+		System.out.println("Answer = " + z);
+		System.out.println("Answer = " + a);
+		System.out.println("Answer = " + b);
+		System.out.println("Answer = " + c);
+		System.out.println("Answer = " + d);
+
+	}
+
+}
